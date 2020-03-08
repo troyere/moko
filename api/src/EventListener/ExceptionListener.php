0bcc1cba89c610df6b8f4bdf0100415babd88805
Tanguy Royere <tanguy.royere@gmail.com>
2020-03-09 00:04:40 +0100

    revert: temporary comments

--- api/src/EventListener/ExceptionListener.php
+++ api/src/EventListener/ExceptionListener.php
@@ -13,31 +13,31 @@ class ExceptionListener
 {
     public function onKernelException(ExceptionEvent $event): void
     {
-//        $e = $event->getThrowable();
-//
-//        $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
-//        $message = $e->getMessage();
-//        $context = [];
-//        $headers = [];
-//
-//        if ($e instanceof HttpExceptionInterface) {
-//            $statusCode = $e->getStatusCode();
-//            $headers = $e->getHeaders();
-//        } else if ($e instanceof JsonDecodeReturnsNullException) {
-//            $statusCode = Response::HTTP_BAD_REQUEST;
-//        } else if ($e instanceof InvalidJsonException) {
-//            $statusCode = Response::HTTP_BAD_REQUEST;
-//            $context = $e->getResult();
-//        }
-//
-//        $data = [];
-//        if (!empty($message)) {
-//            $data['message'] = $message;
-//        }
-//        if (!empty($context)) {
-//            $data['context'] = $context;
-//        }
-//
-//        $event->setResponse(new JsonResponse($data, $statusCode, $headers));
+        $e = $event->getThrowable();
+
+        $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        $message = $e->getMessage();
+        $context = [];
+        $headers = [];
+
+        if ($e instanceof HttpExceptionInterface) {
+            $statusCode = $e->getStatusCode();
+            $headers = $e->getHeaders();
+        } else if ($e instanceof JsonDecodeReturnsNullException) {
+            $statusCode = Response::HTTP_BAD_REQUEST;
+        } else if ($e instanceof InvalidJsonException) {
+            $statusCode = Response::HTTP_BAD_REQUEST;
+            $context = $e->getResult();
+        }
+
+        $data = [];
+        if (!empty($message)) {
+            $data['message'] = $message;
+        }
+        if (!empty($context)) {
+            $data['context'] = $context;
+        }
+
+        $event->setResponse(new JsonResponse($data, $statusCode, $headers));
     }
 }
